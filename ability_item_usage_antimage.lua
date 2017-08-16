bd4916260f0f6d275f51a1b8e715668ce3a2d5db
Thomas Cianfarani <thomas.ci83@gmail.com>
2017-08-16 02:07:47 +0200

    Implémentation d'utilisations basiques des compétences
    Utilisation du Blink pour poursuivre un héro, s'échapper.
    Utilisation de Mana Void dans deux situations: poursuite et teamfights.

--- ability_item_usage_antimage.lua
+++ ability_item_usage_antimage.lua
@@ -5,14 +5,18 @@
 
 -- A considérer:
 --   - Ability build adaptatif.
+--   - Utilisation des items!
+--   - Attention quand il se coince avec Blink.
+--   - Meilleur blink d'évasion.
+--   - Side shop stuck?
 
 ----------------------------------------------------------------------------------------------------
 
 -- Compétences d'antimage
-local abilityManaBreak = "antimage_mana_break";
-local abilityBlink = "antimage_blink";
-local abilitySpellShield = "antimage_spell_shield";
-local abilityManaVoid = "antimage_mana_void";
+local SKILL_Q = "antimage_mana_break";
+local SKILL_W = "antimage_blink";
+local SKILL_E = "antimage_spell_shield";
+local SKILL_ULT = "antimage_mana_void";
 
 -- Talents d'antimage
 local talentLevel10v1 = "special_bonus_hp_150";
@@ -26,25 +30,25 @@ local talentLevel25v2 = "special_bonus_unique_antimage_2";
 
 -- Liste des compétences/talents d'Anti-Mage à levelup dans l'ordre
 local tableAbilitiesLevelUp = {
-    abilityManaBreak, -- lvl 1
-    abilityBlink, -- lvl 2
-    abilityManaBreak, -- lvl 3
-    abilitySpellShield, -- lvl 4
-    abilityManaBreak, -- lvl 5
-    abilityManaVoid, -- lvl 6
-    abilityManaBreak, -- lvl 7
-    abilityBlink, -- lvl 8
-    abilityBlink, -- lvl 9
+    SKILL_Q, -- lvl 1
+    SKILL_W, -- lvl 2
+    SKILL_Q, -- lvl 3
+    SKILL_E, -- lvl 4
+    SKILL_Q, -- lvl 5
+    SKILL_ULT, -- lvl 6
+    SKILL_Q, -- lvl 7
+    SKILL_W, -- lvl 8
+    SKILL_W, -- lvl 9
     talentLevel10v2, -- lvl 10
-    abilityBlink, -- lvl 11
-    abilityManaVoid, -- lvl 12
-    abilitySpellShield, -- lvl 13
-    abilitySpellShield, -- lvl 14
+    SKILL_W, -- lvl 11
+    SKILL_ULT, -- lvl 12
+    SKILL_E, -- lvl 13
+    SKILL_E, -- lvl 14
     talentLevel15v1, -- lvl 15
-    abilitySpellShield, -- lvl 16
-    abilityManaVoid, -- lvl 18
+    SKILL_E, -- lvl 16
+    SKILL_ULT, -- lvl 18
     talentLevel20v2, -- lvl 20
-    talentLevel25v1, -- lvl 25
+    talentLevel25v2, -- lvl 25           -- TODO: Changer en v1 ! (la v2 est pour debug le Mana Void)
 };
 
 -- Récupération du bot sur lequel est exécuté le script
@@ -76,8 +80,13 @@ end
 -- Utilisation des compétences
 ----------------------------------------------------------------------------------------------------
 
+
+castBlinkDesire = 0;
+castManaVoidDesire = 0;
+
 hasBeenCheeky = false;
 
+-- Fonction principale pour l'utilisation des 
 function AbilityUsageThink()
 
     -- Le bot envoie un message à tous les joueurs en début de partie
@@ -85,5 +94,169 @@ function AbilityUsageThink()
 		npcBot:ActionImmediate_Chat("Fuck Magic.", true);
 		hasBeenCheeky = true;
 	end
+
+    -- Récupération des compétences avec leur nom
+    abilityBlink = npcBot:GetAbilityByName(SKILL_W);
+	abilityManaVoid = npcBot:GetAbilityByName(SKILL_ULT);
+    
+    -- Check if we're already using an ability
+	if ( npcBot:IsUsingAbility() ) then return end;
+
+    -- On détermine les désires de blink ou d'ulti du héro
+    castBlinkDesire, castBlinkLocation = ConsiderBlink();
+	castManaVoidDesire, castManaVoidTarget = ConsiderManaVoid();
+
+    -- Si bot désire ulti, alors ulti
+    if (castManaVoidDesire > castBlinkDesire) then 
+        npcBot:Action_UseAbilityOnEntity(abilityManaVoid, castManaVoidTarget);
+        return;
+    end
+
+    -- Si bot désire blink, alors blink
+    if (castBlinkDesire > 0) then
+        npcBot:Action_UseAbilityOnLocation(abilityBlink, castBlinkLocation);
+        return;
+    end
+
+end
+
+----------------------------------------------------------------------------------------------------
+
+function CanCastOffensiveBlink( npcTarget )
+    -- TODO: ajouter d'autres conditions telles que la présence de tour, d'ennemis, d'alliés
+	return npcTarget:CanBeSeen() and not npcTarget:IsInvulnerable() and npcBot:GetHealth() > npcBot:GetMaxHealth() / 2;
+end
+
+function CanCastManaVoidOnTarget( npcTarget )
+    -- TODO: ajouter d'autres conditions telles que la présence de tour, d'ennemis, d'alliés
+	return npcTarget:CanBeSeen() and not npcTarget:IsInvulnerable();
+end
+
+----------------------------------------------------------------------------------------------------
+
+function ConsiderBlink()
+
+    -- Make sure it's castable
+	if (not abilityBlink:IsFullyCastable()) then 
+		return BOT_ACTION_DESIRE_NONE, 0;
+	end;
+
+    -- If we want to cast Ulti at all, bail
+	if ( castManaVoidDesire > 0 ) 
+	then
+		return BOT_ACTION_DESIRE_NONE, 0;
+	end
+
+    -- Get some of its values
+    local nCastRange = abilityBlink:GetCastRange();
+
+    --------------------------------------
+	-- Global high-priorty usage
+	--------------------------------------
+
+    -- Récupération des héros 
+    local tableNearbyEnemyHeroes = npcBot:GetNearbyHeroes( nCastRange, true, BOT_MODE_NONE );
+    for _,npcEnemy in pairs( tableNearbyEnemyHeroes )
+	do
+		if (npcEnemy:IsChanneling() and npcEnemy:GetHealth() < npcEnemy:GetMaxHealth() / 2 and CanCastOffensiveBlink() and not npcBot:IsSilenced()) 
+		then
+            -- Action_AttackUnit(npcEnemy, false);
+			return BOT_ACTION_DESIRE_HIGH, npcEnemy:GetLocation();
+		end
+	end
+
+    --------------------------------------
+	-- Mode based usage
+	--------------------------------------
+
+    -- Mode FARM
+    if ( npcBot:GetActiveMode() == BOT_MODE_FARM ) then
+		local locationAoE = npcBot:FindAoELocation( true, false, npcBot:GetLocation(), nCastRange, 0, 0, 10000 );
+
+		if ( locationAoE.count >= 3 and not npcBot:IsSilenced()) then
+			return BOT_ACTION_DESIRE_LOW, locationAoE.targetloc;
+		end
+	end
+
+    -- Mode RETREAT
+    if ( npcBot:GetActiveMode() == BOT_MODE_RETREAT ) then
+        if (npcBot:DistanceFromFountain() > 2000 and not npcBot:IsSilenced()) then
+            return BOT_ACTION_DESIRE_VERYHIGH, GetAncient(npcBot:GetTeam()):GetLocation();
+        end
+    end
+
+    -- Mode ATTACK
+    if ( npcBot:GetActiveMode() == BOT_MODE_ATTACK ) then
+        
+        local tableNearbyEnemyHeroes = npcBot:GetNearbyHeroes( nCastRange, true, BOT_MODE_NONE );
+        
+        for _,npcEnemy in pairs( tableNearbyEnemyHeroes )
+        do
+            if (npcEnemy:GetHealth() < npcEnemy:GetMaxHealth() / 2 and CanCastOffensiveBlink(npcEnemy) and not npcBot:IsSilenced()) 
+            then
+                return BOT_ACTION_DESIRE_MODERATE, npcEnemy:GetLocation();
+            end
+        end
+    end
+
+    -- TODO: Voir autres modes
+
+
+    return BOT_ACTION_DESIRE_NONE, 0;
+
+end
+
+function ConsiderManaVoid()
+
+    -- Make sure it's castable
+	if ( not abilityManaVoid:IsFullyCastable() ) then 
+		return BOT_ACTION_DESIRE_NONE, 0;
+	end
+
+    -- Récupération de données concernant l'ulti
+    local nCastRange = abilityManaVoid:GetCastRange();
+    local nDmgPerMana = abilityManaVoid:GetSpecialValueFloat( "mana_void_damage_per_mana" );
+
+    -- If a mode has set a target, and we can kill them, do it
+	local npcTarget = npcBot:GetTarget();
+	if ( npcTarget ~= nil and CanCastManaVoidOnTarget(npcTarget) and npcTarget:GetHealth() < (npcTarget:GetMaxMana() - npcTarget:GetMana()) * 1.1 * (1 - npcTarget:GetMagicResist()) )
+	then
+        print("Mana perdu de la cible: ", npcTarget:GetMaxMana() - npcTarget:GetMana());
+        print("Santé de la cible: ", npcTarget:GetHealth());
+        print("Dégats de l'ulti: ", (npcTarget:GetMaxMana() - npcTarget:GetMana()) * nDmgPerMana * (1 - npcTarget:GetMagicResist()));
+		return BOT_ACTION_DESIRE_HIGH, npcTarget;
+	end
+
+    -- Quand teamfight, utiliser sur l'ennemi ayant le plus de mana manquant
+    local tableNearbyAttackingAlliedHeroes = npcBot:GetNearbyHeroes( 1000, false, BOT_MODE_ATTACK );
+	if ( #tableNearbyAttackingAlliedHeroes >= 2 ) 
+	then
+        print("ALERTE TEAMFIGHT!");
+        local mostDamageableEnemy = nil;
+        local mostManaPointsLost = 0;
+
+        -- On cherche l'ennemi ayant perdu le plus de mana
+        local tableNearbyEnemyHeroes = npcBot:GetNearbyHeroes( nCastRange, false, BOT_MODE_NONE );
+        for _,npcEnemy in pairs( tableNearbyEnemyHeroes )
+        do
+            if (CanCastManaVoidOnTarget(npcEnemy)) then
+                print("On examine un ennemi!");
+                lostManaForCurrentEnemy = npcEnemy:GetMaxMana() - npcEnemy:GetMana();
+                if (lostManaForCurrentEnemy > mostManaPointsLost and lostManaForCurrentEnemy > npcEnemy:GetMaxMana() / 2) then
+                    mostDamageableEnemy = npcEnemy;
+                    print("On a une cible!");
+                end
+            end
+        end
+
+        -- On va utiliser l'ulti sur l'ennemi ayant le plus de mana manquant et étant tuable avec l'ulti
+        if ( mostDamageableEnemy ~= nil )
+        then
+            print("AM a trouvé l'homme à abattre! ULTI!!!")
+            return BOT_ACTION_DESIRE_HIGH, mostDamageableEnemy;
+        end
+    end
+
+    return BOT_ACTION_DESIRE_NONE, 0;
 
 end
--- hero_selection.lua
+++ hero_selection.lua
@@ -13,7 +13,7 @@ function Think()
 
     if ( GetTeam() == TEAM_RADIANT ) then
         SelectHero( 1, "npc_dota_hero_lina" );
-        SelectHero( 2, "npc_dota_hero_antimage" );
+        SelectHero( 2, "npc_dota_hero_nevermore" );
         SelectHero( 3, "npc_dota_hero_bloodseeker" );
         SelectHero( 4, "npc_dota_hero_crystal_maiden" );
     elseif ( GetTeam() == TEAM_DIRE ) then
@@ -21,7 +21,7 @@ function Think()
         SelectHero( 6, "npc_dota_hero_earthshaker" );
         SelectHero( 7, "npc_dota_hero_juggernaut" );
         SelectHero( 8, "npc_dota_hero_mirana" );
-        SelectHero( 9, "npc_dota_hero_nevermore" );
+        SelectHero( 9, "npc_dota_hero_antimage" );
     end
  
 end 
